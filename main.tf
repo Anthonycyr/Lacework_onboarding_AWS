86c8403a6a338a538d8277ce9348ad064cbd1888
Anthony Cyr <acyr@egyde.ca>
2023-02-27 16:30:43 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "lacework" {}
+
+module "aws_config" {
+  source  = "lacework/config/aws"
+  version = "~> 0.5"
+  iam_role_name = "LaceworkRole"
+}
+
+module "aws_cloudtrail" {
+  source  = "lacework/cloudtrail/aws"
+  version = "~> 2.0"
+
+  bucket_force_destroy  = true
+  use_existing_iam_role = true
+  iam_role_name         = module.aws_config.iam_role_name
+  iam_role_arn          = module.aws_config.iam_role_arn
+  iam_role_external_id  = module.aws_config.external_id
+}
